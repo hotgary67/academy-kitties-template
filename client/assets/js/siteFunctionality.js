05765c817c8eba9d86492d21b8325e9c5d9e0e7a
hotgary67 <69466703+hotgary67@users.noreply.github.com>
2020-10-05 14:37:06 +0200

    Add files via upload

--- client/assets/js/siteFunctionality.js
+++ client/assets/js/siteFunctionality.js
@@ -1,50 +1,56 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function() {
+  console.log("ready!");
 });
 document.getElementById("randomkitty").addEventListener("click", randomKittyButtonClick);
+//document.getElementById("createKitty").addEventListener("click", createKittyButton);
+
 
 function randomKittyButtonClick() {
   document.getElementById("randomkitty").innerHTML = "Click to keep randomizing";
   console.log("randomizzzeed")
 }
 
+//this function creates the generated Kitty and stored it on the blockchain
+//function createKittyButton() {
+ // document.getElementById("createKitty").innerHTML = "Kitty request to blockchain send....";
+ // console.log("kitty creation started")
+//}
 
-var btn = document.getElementById("randomkitty");
+var randomKittyBtn = document.getElementById("randomkitty");
+//var createKittyBtn = document.getElementById("createKitty");
 
-    // Assigning event listeners to the button
-    btn.addEventListener("click", randomHatColor);
-    btn.addEventListener("click", randomHeadColor);
-    btn.addEventListener("click", randomEyesColor);
-    btn.addEventListener("click", randomEarColor);
-    btn.addEventListener("click", randomArmsLegsColor);
-    btn.addEventListener("click", randomBellyColor);
-    btn.addEventListener("click", randomTailColor);
-    btn.addEventListener("click", randomTrunkColor);
-    btn.addEventListener("click", randomVaseColor);
-    btn.addEventListener("click", randomEyeVariation);
-    btn.addEventListener("click", randomDecorationNailVariation);
-    btn.addEventListener("click", randomDecorationWings);
-    btn.addEventListener("click", randomAnimationVariation);
+// Assigning event listeners to the random kitty button
+randomKittyBtn.addEventListener("click", randomHatColor);
+randomKittyBtn.addEventListener("click", randomHeadColor);
+randomKittyBtn.addEventListener("click", randomEyesColor);
+randomKittyBtn.addEventListener("click", randomEarColor);
+randomKittyBtn.addEventListener("click", randomArmsLegsColor);
+randomKittyBtn.addEventListener("click", randomBellyColor);
+randomKittyBtn.addEventListener("click", randomTailColor);
+randomKittyBtn.addEventListener("click", randomTrunkColor);
+randomKittyBtn.addEventListener("click", randomVaseColor);
+randomKittyBtn.addEventListener("click", randomEyeVariation);
+randomKittyBtn.addEventListener("click", randomDecorationNailVariation);
+randomKittyBtn.addEventListener("click", randomDecorationWings);
+randomKittyBtn.addEventListener("click", randomAnimationVariation);
+
+// Assigning event listeners to the create Kitty Button
+//createKittyBtn.addEventListener("click", getDna);
+//createKittyBtn.addEventListener("click", instance);
 
 
-
-
-
-//GAAN NOG DINGEN MIS MET DE RANDOMIZER, GETALLEN GROTER DAN 100 KOMEN ERUIT + ER ZIT EEN GEKKE LOOP IN ERGENS
 
 function filterTentoHunderd() {
-    let numberTenToHunderd = Math.floor(Math.random() * 100) + 10;
-    return numberTenToHunderd;
+  let numberTenToHunderd = Math.floor(Math.random() * 90) + 10;
+  return numberTenToHunderd;
 }
-console.log(filterTentoHunderd())
-
-function filterOnetoSix() {
-    let numberTenToHunderd = Math.floor(Math.random() * 6) + 1;
-    return numberTenToHunderd;
-}
-
 
 function filterOnetoFive() {
-    let numberTenToHunderd = Math.floor(Math.random() * 5) + 1;
-    return numberTenToHunderd;
+  let numberTenToHunderd = Math.floor(Math.random() * 5) + 1;
+  return numberTenToHunderd;
+}
+
+function filterOnetoSix() {
+  let numberTenToHunderd = Math.floor(Math.random() * 6) + 1;
+  return numberTenToHunderd;
 }
