da9d7bb4003403fa7ab0c6d66df32b55457a7e87
hotgary67 <69466703+hotgary67@users.noreply.github.com>
2020-10-07 15:18:25 +0200

    Update siteFunctionality.js

--- client/assets/js/siteFunctionality.js
+++ client/assets/js/siteFunctionality.js
@@ -1,44 +1,30 @@
-$(document).ready(function() {
+$(document).ready(function () {
   console.log("ready!");
 });
-document.getElementById("randomkitty").addEventListener("click", randomKittyButtonClick);
-//document.getElementById("createKitty").addEventListener("click", createKittyButton);
+document
+  .getElementById("randomkitty")
+  .addEventListener("click", randomKittyButtonClick);
 
 
 function randomKittyButtonClick() {
-  document.getElementById("randomkitty").innerHTML = "Click to keep randomizing";
-  console.log("randomizzzeed")
+  document.getElementById("randomkitty").innerHTML =
+    "Click to keep randomizing";
+  randomHatColor();
+  randomHeadColor();
+  randomEyesColor();
+  randomEarColor();
+  randomArmsLegsColor();
+  randomBellyColor();
+  randomTailColor();
+  randomTrunkColor();
+  randomVaseColor();
+  randomVaseColor();
+  randomEyeVariation();
+  randomDecorationNailVariation();
+  randomDecorationWings();
+  randomAnimationVariation();
+  console.log("randomizzzeed");
 }
-
-//this function creates the generated Kitty and stored it on the blockchain
-//function createKittyButton() {
- // document.getElementById("createKitty").innerHTML = "Kitty request to blockchain send....";
- // console.log("kitty creation started")
-//}
-
-var randomKittyBtn = document.getElementById("randomkitty");
-//var createKittyBtn = document.getElementById("createKitty");
-
-// Assigning event listeners to the random kitty button
-randomKittyBtn.addEventListener("click", randomHatColor);
-randomKittyBtn.addEventListener("click", randomHeadColor);
-randomKittyBtn.addEventListener("click", randomEyesColor);
-randomKittyBtn.addEventListener("click", randomEarColor);
-randomKittyBtn.addEventListener("click", randomArmsLegsColor);
-randomKittyBtn.addEventListener("click", randomBellyColor);
-randomKittyBtn.addEventListener("click", randomTailColor);
-randomKittyBtn.addEventListener("click", randomTrunkColor);
-randomKittyBtn.addEventListener("click", randomVaseColor);
-randomKittyBtn.addEventListener("click", randomEyeVariation);
-randomKittyBtn.addEventListener("click", randomDecorationNailVariation);
-randomKittyBtn.addEventListener("click", randomDecorationWings);
-randomKittyBtn.addEventListener("click", randomAnimationVariation);
-
-// Assigning event listeners to the create Kitty Button
-//createKittyBtn.addEventListener("click", getDna);
-//createKittyBtn.addEventListener("click", instance);
-
-
 
 function filterTentoHunderd() {
   let numberTenToHunderd = Math.floor(Math.random() * 90) + 10;
